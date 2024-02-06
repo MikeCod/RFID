0acd5405991e533c0f6e9e641bc539f7a4181e41
Aidasaoudi <saoudi_aida@yahoo.fr>
2024-02-06 18:26:40 +0100

    fix some errors

---
+++ code.js
@@ -0,0 +1,21 @@
+const form = {
+    "name": "Nightmare",
+    "email": "mike@dark.org",
+    "password": "a39582b95b893d5042d946af0029bb6efdd3dca2dc17be8474920278684a84aa",
+    "phone": "+33666666666",
+    "form": {
+        "target_1": 1,
+        "target-area": 0,
+        "doors": 1,
+        "critical": 1,
+        "security-type": 0,
+        "technology": 1,
+        "permissions": 4,
+        "permissions-revokation": 0,
+        "permissions-revokation-recent": 1,
+        "enterprise-size": 3,
+        "enterprise-domain": 2,
+        "enterprise-security-level": 2
+    }
+}
+
