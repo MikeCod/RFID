f5461a89db32ecbd6492c9863171733f32bd7315
MikeCod <mike.cod013@gmail.com>
2024-02-06 14:55:28 +0100

    fix: generic questions

--- src/asset/question/index.js
+++ src/asset/question/index.js
@@ -1,10 +1,9 @@
 
-import generic from "./generic.json";
 import data from "./data.json";
 import area from "./area/area.json";
 import tickets from "./area/tickets.json";
 import company from "./area/company/company.json";
 
 
-export const questions = [generic, data, area, tickets, company];
-export { generic, data, area, tickets, company };
+export const questions = [data, area, tickets, company];
+export { data, area, tickets, company };
